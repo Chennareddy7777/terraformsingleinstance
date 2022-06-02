ed2740391e79e8b9d8d3ec12b9c724282ef7f2a5
Chennareddy7777 <94470941+Chennareddy7777@users.noreply.github.com>
2022-06-02 11:31:02 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,10 +4,10 @@ variable "aws_region" {}
 variable "amis" {
     description = "AMIs by region"
     default = {
-        us-east-1 = "ami-97785bed" # ubuntu 14.04 LTS
-		us-east-2 = "ami-f63b1193" # ubuntu 14.04 LTS
-		us-west-1 = "ami-824c4ee2" # ubuntu 14.04 LTS
-		us-west-2 = "ami-f2d3638a" # ubuntu 14.04 LTS
+        us-east-1 = "ami-0c4f7023847b90238" # ubuntu 20.04 LTS
+		us-east-2 = "ami-0eea504f45ef7a8f7" # ubuntu 20.04 LTS
+		us-west-1 = "ami-0487b1fe60c1fd1a2" # ubuntu 20.04 LTS
+		us-west-2 = "ami-0cb4e786f15603b0d" # ubuntu 20.04 LTS
     }
 }
 variable "vpc_cidr" {}
